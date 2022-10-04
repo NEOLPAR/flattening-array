b116cd404258cab3f94cb205586f0e986d04db29
NEOLPAR <ricardo@neolpar.com>
2022-10-04 23:05:09 +0100

    tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,56 @@
-describe('App setup', () => {
-    test('app setup successfully', () => {
-        const actual = true
+const {flat} = require('./index')
 
-        expect(actual).toBeTruthy()
+describe('flat function', () => {
+    test('simple array', () => {
+        const actual = [1, 2, 3]
+        const expected = [1, 2, 3]
+
+        expect(flat(actual)).toStrictEqual(expected)
+    })
+
+    test('complex array with depth', () => {
+        const actual = [1, 2, 3, [1, 2, ['a', {},6,[7,[],9]]],[9,8]]
+        const expected = [1, 2, 3, 1, 2, 'a', {}, 6, 7, 9, 9, 8]
+
+
+        expect(flat(actual)).toStrictEqual(expected)
+    })
+
+    describe('edges', () => {
+        test('no input param', () => {
+            const expected = undefined
+
+            expect(flat()).toStrictEqual(expected)
+        })
+        test('empty array input', () => {
+            const actual = []
+            const expected = []
+
+            expect(flat(actual)).toStrictEqual(expected)
+        })
+        test('number input', () => {
+            const actual = 1
+            const expected = 1
+
+            expect(flat(actual)).toStrictEqual(expected)
+        })
+        test('char input', () => {
+            const actual = 'a'
+            const expected = 'a'
+
+            expect(flat(actual)).toStrictEqual(expected)
+        })
+        test('object input', () => {
+            const actual = {}
+            const expected = {}
+
+            expect(flat(actual)).toStrictEqual(expected)
+        })
+        test('null input', () => {
+            const actual = null
+            const expected = null
+
+            expect(flat(actual)).toStrictEqual(expected)
+        })
     })
 })
